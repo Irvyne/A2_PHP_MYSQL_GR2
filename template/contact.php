030d2780640298537658643697b94ab0192dbd14
Thibaud BARDIN <thibaud.bardin@gmail.com>
2014-10-10 11:39:13 +0200

    Update

--- template/contact.php
+++ template/contact.php
@@ -1,5 +1,15 @@
 <?php include __DIR__.'/_header.php'; ?>
 
-<h1>Contact</h1>
+    <h1>Contact</h1>
+
+    <form action="" method="post">
+        <label for="name">Name: </label>
+        <input id="name" name="name" type="text">
+        <br>
+        <label for="content">Content: </label>
+        <textarea name="content" id="content" cols="30" rows="10"></textarea>
+        <br>
+        <input name="submitContact" type="submit">
+    </form>
 
 <?php include __DIR__.'/_footer.php'; ?>
